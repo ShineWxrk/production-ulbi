8d6dbe2f41b02e2c7b722cdd48e8e43410098a42
ShineWxrk <shinewxrk@gmail.com>
2024-02-06 20:49:13 +0300

    add not found page

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,21 +1,25 @@
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
 import { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { routeConfig } from 'shared/config/routeConfig/routeConfig';
+import { PageLoader } from 'widgets/PageLoader';
 
 const AppRouter = () => {
     return (
-    <Suspense fallback={'Loading'}>
         <Routes>
             {Object.values(routeConfig).map(({element, path}) => (
-                <Route key={path} path={path} element={
-                    <div className='page-wrapper'>
-                        {element}
-                    </div>}/>
-            ))}
+                <Route 
+                key={path} 
+                path={path} 
+                element={(
+                    <Suspense fallback={<PageLoader/>}>
+                        <div className='page-wrapper'>
+                            {element}
+                        </div>
+                    </Suspense>    
+                )}
+            />
+        ))}
         </Routes>
-    </Suspense>
     );
 };
 
